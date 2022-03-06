75d7af10ec0ce4eb6ce1b2b2c75998a5e409aee7
Behruz-01 <bekhruzgaybullaev@gmail.com>
2022-03-06 21:47:40 +0500

    initialy commit

---
+++ main.js
@@ -0,0 +1,42 @@
+//  var numbers = [1,3,0,5,0,7,0];
+
+//  function zeroEnd(numbers){
+//      var number = [];
+//      for (var num of numbers){
+//          if(num=0){
+//              number.unshift()
+//          }
+//      }
+//      return number
+//  }
+
+//  console.log(zeroEnd());
+
+// var numbers = [1,2,0,-1,-2,3,4];
+
+// function zero(){
+
+//     var number = [];
+
+//     for (var num of numbers) {
+//         if(num<0){
+//             number.push
+//         }
+//     }
+// }
+
+
+var numbers=[1,2,0,-1,-2,3,4]
+
+
+var arr = numbers[0]
+console.log(arr)
+function numbersEl(){
+    var sum=0;
+    for(var i=0 ;i<=numbers.length; i++){
+        if(numbers[i]>=0){
+            console.log("musbat")
+        return sum +=numbers[i].value
+        }
+    }   
+}
